53ee139d38d642584494fee0c88995dc9d7ff5c9
Aadi <adithyamanikumar@proton.me>
2024-06-21 20:20:03 +0530

    Added the works page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//////////////////////////// Toggling the small screen menu button
 
 const menuBtn = document.querySelector('#sm-menu-btn');
 const smallScreenNav = document.querySelector('#sm-screen-nav');
@@ -11,3 +12,27 @@ document.addEventListener('click', (e) => {
 		smallScreenNav.style.display = "none"
 	}
 })
+
+/////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////// Video Play on Mouse over
+
+document.addEventListener('mouseover', (e) => {
+	if(e.target.dataset.video) {
+		e.target.currentTime = 0;
+		e.target.play();
+	};
+})
+
+/////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////// Video Play on Mouse over
+
+document.addEventListener('mouseout', (e) => {
+	if(e.target.dataset.video) {
+		e.target.pause();
+	};
+})
+
+/////////////////////////////////////////////////////////////////
